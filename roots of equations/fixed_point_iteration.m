efd54af57515228bbcfc7bdeb33209b54213f86f
Bumsoo Kim <cmng828rhuypqq@cau.ac.kr>
2021-10-12 18:07:15 +0900

    Update fixed_point_iteration.m

--- roots of equations/fixed_point_iteration.m
+++ roots of equations/fixed_point_iteration.m
@@ -2,13 +2,38 @@
 
 clc, clear;
 
+f1 = figure;
+f2 = figure;
+
 %%  Initial Variable
 
 fm = @(x) exp(-x);
 
 %% main code : Fixed Point Iteration
 
+figure(f1);
+
 [x, ea] = fixed_pt_sys(fm, 1, 10^(-3), 100);
+
+% draw graph
+x_idx = 0:0.1:10;
+plot(x_idx, exp(-x_idx));
+title('Fixed Point Iteration Method');
+xlabel('x');
+ylabel('y');
+xlim([0 10]);
+ylim([0 10]);
+hold on
+
+% draw root line
+plot([0 10], [0 10]);
+
+% draw fixed point iteration method root
+plot(x, fm(x), 'ob');
+legend('g(x) = exp(-x)', 'f(x) = x', 'root of fixed point iteration');
+
+% draw error
+figure(f2);
 
 xx = 1:length(ea);
 plot(xx, ea, 'or');
